2809b613e6da2574c9edbf36c6befb21238d724a
Ata00422 <167747030+Ata00422@users.noreply.github.com>
2025-04-24 19:07:07 +0500

    changes

--- src/ApiEndpoint.js
+++ src/ApiEndpoint.js
@@ -1,3 +1,3 @@
-const apiUrl = "http://localhost:8000"
+const apiUrl = "https://asset-download-backend.vercel.app"
 // const apiUrl = "https://sell-my-app-backend.vercel.app"
 export default apiUrl;
