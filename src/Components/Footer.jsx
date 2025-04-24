ec3a12a02836caefb5b7b99aaa37c0f077ea99ad
Ata00422 <167747030+Ata00422@users.noreply.github.com>
2025-04-24 19:18:49 +0500

    changes

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGlobe, faPhone } from "@fortawesome/free-solid-svg-icons";
+import { faGlobe } from "@fortawesome/free-solid-svg-icons";
 import { faFacebookF, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons';
 import { Col, Container, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
@@ -15,8 +15,8 @@ export default function Footer() {
                                 <Col md={3}>
                                     <h3>LOGO</h3>
                                     <Link to="/"><p className="mb-3">Home</p></Link>
-                                    <Link to="/"><p className="mb-3">Categories</p></Link>
-                                    <Link to="/"><p className="mb-3">About</p></Link>
+                                    <Link to="/about"><p className="mb-3">About</p></Link>
+                                    <Link to="/contact"><p className="mb-3">Contact</p></Link>
                                     <div className="text-white d-flex">
                                         <p className="me-3">Copyright</p>
                                         <p>Terms Of Service</p>
@@ -27,10 +27,6 @@ export default function Footer() {
                                     <div className="d-flex text-white align-items-center mb-3">
                                         <FontAwesomeIcon icon={faGlobe} className="me-3" />
                                         <p>mywebsite@gmail.com</p>
-                                    </div>
-                                    <div className="d-flex text-white align-items-center mb-3">
-                                        <FontAwesomeIcon icon={faPhone} className="me-3" />
-                                        <p>+92 314 0000000</p>
                                     </div>
                                     <div className="d-flex justify-content-center text-white">
                                         <FontAwesomeIcon icon={faFacebookF} className="me-3" />
